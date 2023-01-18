fa78dd4614f4dcefc3d49a65092b06fc990ad13f
Christian <xtianluz@gmail.com>
2023-01-19 05:49:27 +0800

    building layout

--- app/src/main/java/com/example/googleapi/screen/booksearch/BookSearcScreen.kt
+++ app/src/main/java/com/example/googleapi/screen/booksearch/BookSearcScreen.kt
@@ -1,8 +1,90 @@
 package com.example.googleapi.screen.booksearch
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import com.example.googleapi.R
+import com.example.googleapi.screen.booksearch.DataSamples.SampleImageOne
+import com.example.googleapi.ui.theme.GoogleApiTheme
 
 @Composable
-fun BookSearchScreen(){
+fun BookSearchScreen(searchViewModel: BookSearchViewModel = viewModel()){
+    Column {
+        SearchBar(
+            userInput = searchViewModel.userInput,
+            onUserInputChange = { searchViewModel.updateUserInput(it) }
+        )
+        SearchResult()
+    }
+}
 
+@Composable
+fun SearchBar(
+    userInput: String,
+    onUserInputChange: (String) -> Unit,
+){
+    Box(
+        contentAlignment = Alignment.TopCenter,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            value = userInput,
+            onValueChange = onUserInputChange,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large
+        )
+    }
+}
+
+@Composable
+fun SearchResult(){
+    BookThumbnail()
+}
+
+@Composable
+fun BookThumbnail(){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(360.dp)
+            .padding(16.dp),
+        elevation = 16.dp
+    ){
+        AsyncImage(
+            model = SampleImageOne,
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            error = painterResource(id = R.drawable.ic_connection_error),
+            placeholder = painterResource(id = R.drawable.ic_broken_image),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+    }
+}
+
+
+/////////////////////////////////////////////////////////////////
+////////        PREVIEW COMPOSABLE           ////////////////////
+/////////////////////////////////////////////////////////////////
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview(){
+    GoogleApiTheme() {
+        SearchBar(userInput = "Placeolder", onUserInputChange = { } )
+    }
 }
--- app/src/main/java/com/example/googleapi/screen/booksearch/BookSearchViewModel.kt
+++ app/src/main/java/com/example/googleapi/screen/booksearch/BookSearchViewModel.kt
@@ -1,2 +1,15 @@
 package com.example.googleapi.screen.booksearch
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class BookSearchViewModel : ViewModel(){
+    var userInput by mutableStateOf("")
+        private set
+
+    fun updateUserInput(userNewInput: String){
+        userInput = userNewInput
+    }
+}
