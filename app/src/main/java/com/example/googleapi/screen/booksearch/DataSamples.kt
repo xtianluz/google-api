a6b72ea6ee54b0d11ccf6bc1d6b98c038f4ba326
Christian <xtianluz@gmail.com>
2023-01-25 09:38:06 +0800

    some deletion

--- app/src/main/java/com/example/googleapi/screen/booksearch/DataSamples.kt
+++
@@ -1,7 +0,0 @@
-package com.example.googleapi.screen.booksearch
-
-object DataSamples {
-    const val SampleImageOne = "https://books.google.com/books/content?id=022xAAAAIAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73Vt90r2zIc8RzUSv4rT2-J4FCBSo8IThJSo5lIPUpxiDjxo77YOWTLVeBXKTnweJ_xprwj1TyJc_OdJTt9PNCgJIWgV3jwBxFdXQ36BihNroJresv32vjpOu5_mL2U9Ko5MmD0&source=gbs_api"
-    const val BaseSample = "https://www.googleapis.com/books/v1/volumes?q="
-
-}
