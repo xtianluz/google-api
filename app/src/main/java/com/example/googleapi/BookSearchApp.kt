e23992d0bc11e5f0f93000715dc2d34aca80a138
Christian <xtianluz@gmail.com>
2023-01-26 18:49:28 +0800

    getting null...

--- app/src/main/java/com/example/googleapi/BookSearchApp.kt
+++ app/src/main/java/com/example/googleapi/BookSearchApp.kt
@@ -8,5 +8,5 @@ import com.example.googleapi.screen.booksearch.BookSearchViewModel
 @Composable
 fun BookSearchApp(searchViewModel: BookSearchViewModel){
 
-    BookSearchScreen(searchViewModel)
+    BookSearchScreen(searchViewModel.searchUiState, searchViewModel)
 }
